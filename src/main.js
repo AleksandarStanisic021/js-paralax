29b2b3d22622ad6663dc2eb092c4d5359f3fc678
ciandre1976 <ciandre23@gmail.com>
2026-03-01 16:44:08 +0100

    fixes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
 import "./style.css";
 
-console.log("yiyi");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+console.log(canvas);
+console.log(ctx);
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
